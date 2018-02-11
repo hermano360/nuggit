40e10c8ab095107fe424ed3148417625737fd9ee
Thuong Ho <thuong.t.ho@gmail.com>
2018-02-11 09:35:30 -0800

    rebase with master

--- assets/js/cool-stuff.js
+++ assets/js/cool-stuff.js
@@ -1,16 +1,20 @@
 function activateAR () {
   var _scanner = document.getElementById('scanner');
-  console.log('hiding the madness', _scanner);
+  console.log('hiding the madness');
 
   var _wrapper = document.getElementById('wrapper');
   _wrapper.classList.add('fade-out');
-  // _wrapper.style.visibility = 'hidden';
 
 
-  var _arStuff = document.createElement('div');
-  _arStuff.setAttribute('id', 'container');
-  console.log('document', document);
-  document.body.appendChild(_arStuff);
+  var _arStuffContainer = document.createElement('div');
+  _arStuffContainer.setAttribute('id', 'container');
+
+  var _arStuffIndicator = document.createElement('div');
+  var _arStuffIndicatorText = document.createTextNode('a');
+
+  _arStuffIndicator.appendChild(_arStuffIndicatorText);
+  _arStuffContainer.appendChild(_arStuffIndicator);
+  document.body.appendChild(_arStuffContainer);
 
   var _script = document.createElement('script');
   _script.setAttribute('type', 'text/javascript');
